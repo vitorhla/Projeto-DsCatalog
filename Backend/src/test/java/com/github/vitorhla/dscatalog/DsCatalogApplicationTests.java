578cc22e7421433a47d15372fbe8ca16043b6bd9
vitorhla <vitorhenriquealt@gmail.com>
2021-05-13 15:09:13 -0300

    Project created

---
+++ Backend/src/test/java/com/github/vitorhla/dscatalog/DsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitorhla.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
