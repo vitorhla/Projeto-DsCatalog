ce16f9fcafa68c079d77968df50566f92bbb76cf
vitorhla <vitorhenriquealt@gmail.com>
2021-05-14 14:43:36 -0300

    Database Seeding

--- Backend/src/main/java/com/github/vitorhla/dscatalog/services/CategoryService.java
+++ Backend/src/main/java/com/github/vitorhla/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.vitorhla.dscatalog.entities.Category;
 import com.github.vitorhla.dscatalog.repositories.CategoryRepository;
@@ -14,10 +15,13 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return categoryRepository.findAll();
 		
 	}
+	
+	
 	
 	
 	
