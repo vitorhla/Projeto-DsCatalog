0ada6167590942e9867cd7b2e4799374923c23c1
vitorhla <vitorhenriquealt@gmail.com>
2021-05-15 20:09:52 -0300

    POST Category

--- Backend/src/main/java/com/github/vitorhla/dscatalog/controllers/CategoryController.java
+++ Backend/src/main/java/com/github/vitorhla/dscatalog/controllers/CategoryController.java
@@ -1,13 +1,17 @@
 package com.github.vitorhla.dscatalog.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.vitorhla.dscatalog.dto.CategoryDTO;
 import com.github.vitorhla.dscatalog.services.CategoryService;
@@ -33,6 +37,15 @@ public class CategoryController {
 		
 	}
 	
+	@PostMapping
+	public ResponseEntity <CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = categoryService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
+		
+	}
 	
 
 }
--- Backend/src/main/java/com/github/vitorhla/dscatalog/services/CategoryService.java
+++ Backend/src/main/java/com/github/vitorhla/dscatalog/services/CategoryService.java
@@ -36,6 +36,15 @@ public class CategoryService {
 		
 		
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity =  new Category();
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 	
 	
 	
