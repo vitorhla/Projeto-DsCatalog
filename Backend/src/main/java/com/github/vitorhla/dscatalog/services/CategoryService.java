8afca5cb0a2c2a48da141d93bcf8b188c4325836
vitorhla <vitorhenriquealt@gmail.com>
2021-05-15 17:19:32 -0300

    DTO

--- Backend/src/main/java/com/github/vitorhla/dscatalog/services/CategoryService.java
+++ Backend/src/main/java/com/github/vitorhla/dscatalog/services/CategoryService.java
@@ -1,11 +1,15 @@
 package com.github.vitorhla.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.vitorhla.dscatalog.dto.CategoryDTO;
 import com.github.vitorhla.dscatalog.entities.Category;
 import com.github.vitorhla.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +20,10 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return categoryRepository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list  = categoryRepository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+			
 		
 	}
 	
