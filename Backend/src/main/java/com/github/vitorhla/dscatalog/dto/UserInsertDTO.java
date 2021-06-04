70d4a572360ae773b909e2310a68f7455cc0aae6
vitorhla <vitorhenriquealt@gmail.com>
2021-06-04 20:52:14 -0300

    Custom ConstraintValidator

--- Backend/src/main/java/com/github/vitorhla/dscatalog/dto/UserInsertDTO.java
+++ Backend/src/main/java/com/github/vitorhla/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.github.vitorhla.dscatalog.dto;
 
+import com.github.vitorhla.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID =1L;
 	
